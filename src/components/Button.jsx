f9080971bbb46d888c7b0799c8880201d25798fc
CarmenCruces <karmelacfer@gmail.com>
2023-05-08 20:28:54 +0200

    First Commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/Button.css';
+
+function Button(props) {
+    return (
+        <>
+            <button className={`basic-btn ${props.clase}`}>{props.text}</button>
+        </>
+        
+    );
+}
+
+export default Button;
---
+++ src/components/Counter.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import '../styles/Button.css';
+
+function Counter(props) {
+
+    const [number, setNumber] = useState(0);
+
+    //number es el valor de mi estado
+    //setNumber es lo que hace que se cambie
+    //useState (el valor inicial)
+
+    const counter = () => {
+        setNumber( number + 1); 
+    }
+
+    return (
+        
+    <>
+            <button className={`basic-btn ${props.clase}`}
+            onClick={counter}>{props.text}  {number}</button>
+    </>
+    )
+}
+
+export default Counter;
+
+Counter.defaultProps = {
+    text: '💓'
+}
